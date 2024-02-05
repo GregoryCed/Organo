a84cb745902cfdf508c11638bd0ad2452a873431
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-05 11:56:25 -0300

    Corrigindo Bugs e finalizando o projeto

--- src/App.js
+++ src/App.js
@@ -47,8 +47,7 @@ function App() {
 
   const [colaboradores, setColaboradores] = useState([])
 
-  const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
+  const aoNovoColaboradorAdicionado = (colaborador) => {    
     setColaboradores([...colaboradores, colaborador])
   }
 
